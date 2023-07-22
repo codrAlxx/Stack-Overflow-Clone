d07d971b8274ab397b852561a51bcda93921ef2c
codealxx <alok02jha@gmail.com>
2023-07-22 13:19:36 +0530

    Feat: Changes

--- client/src/pages/Questions/DisplayAnswer.jsx
+++ client/src/pages/Questions/DisplayAnswer.jsx
@@ -1,36 +1,44 @@
 import React from 'react'
-import moment from 'moment'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useLocation, useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-
+import moment from 'moment'
+import copy from 'copy-to-clipboard'
 import Avatar from '../../components/Avatar/Avatar'
 import { deleteAnswer } from '../../actions/question'
 
-const DisplayAnswer = ({question, handleShare}) => {
+const DisplayAnswer = ({question}) => {
 
     const User = useSelector((state) => (state.currentUserReducer))
-    const { id } = useParams()
-    const dispatch = useDispatch()
-    const handleDelete = (answerId, noOfAnswers) => {
-        dispatch(deleteAnswer(id, answerId, noOfAnswers - 1))
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const url = "http://localhost:3000"
+    const handleShare = () => {
+        copy(url + location.pathname)
+        alert("Copied URL to clipboard!")
     }
+
+    const handleDelete = (answerId, noOfAnswers) => {
+        dispatch(deleteAnswer(id, answerId, noOfAnswers - 1));
+    }
+
     return (
         <div>
             {
                 question.answer.map((ans) => (
-                    <div className="display-ans" key={ans._id}>
+                    <div className='display-ans' key={ans._id}>
                         <p>{ans.answerBody}</p>
                         <div className="question-actions-user">
                             <div>
                                 <button type="button" onClick={handleShare}>Share</button>
                                 {
                                     User?.result?._id === ans?.userId && (
-                                        <button type='button' onClick={() => handleDelete(ans._id, question.noOfAnswers )}>Delete</button>
+                                        <button type='button' onClick={() => handleDelete(ans._id, question.noOfAnswers)}>Delete</button>
                                     )
                                 }
                             </div>
                             <div>
-                                <p>answered {moment(ans.answeredOn).fromNow()}</p>
+                                <p>answered on {moment(ans.answeredOn).fromNow()}</p>
                                 <Link to={`/Users/${ans.userId}`} className='user-link' style={{color:'#0086d8'}}>
                                     <Avatar backgroundColor="lightgreen" px='8px' py='5px' borderRadius='4px'>{ans.userAnswered.charAt(0).toUpperCase()}</Avatar>
                                     <div>
